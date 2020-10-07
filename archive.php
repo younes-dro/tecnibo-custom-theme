cc8ddc759b3f7596c844a0c748f88f916dfb3c5b
younes-dro <dro66_8@hotmail.fr>
2020-10-07 19:11:50 +0100

    Add related projects to the category

--- archive.php
+++ archive.php
@@ -22,19 +22,24 @@ get_header(); ?>
                                 $taxonomy_id = $wp_query->queried_object->term_id;
                                 $taxonomy_image_id = get_term_meta($taxonomy_id, 'showcase-taxonomy-image-id', true);
                                 $taxonomy_image_url = wp_get_attachment_image_url($taxonomy_image_id,'full');
+                                
                             ?>
+                            <?php if ( $taxonomy_image_url) :?>
                             <header class="custom_tax_header" style="background-image: url('<?php echo $taxonomy_image_url ?>')">
                                 <h1 class="custom-tax-title"><?php  single_cat_title(''); ?></h1>
                                 
                             </header>
+                            <?php endif;?>
                             <?php the_archive_description('<section class="archive-description">', '</section>'); ?>
                             <?php
 
                                 
                             ?>
+                            
                             <?php if ( have_posts() ) : ?>
                                 <?php if ( $wp_query->queried_object->taxonomy == 'product_category' ): ?>
                                     <?php 
+                                    // Products 
                                     $args = array(
                                         'post_type' => 'tecnibo_product',
                                         'orderby' => 'title',
@@ -46,15 +51,46 @@ get_header(); ?>
                                                 'terms' => $wp_query->queried_object->term_id //6 //198
                                             )
                                         ),
-                                    );                                    
-                                    echo Tecnibo_Portfolio::get_grid_products_projects( $args );
+                                    );
+                                    if ( Tecnibo_Portfolio::category_has_product_project($args)){
+                                        echo '<div class="divider"><hr class="flush"></div>';
+                                        echo '<section class="tecnibo-row">';
+                                        echo '<h2 class="related_products_projects"><span>' . __('Products','tecnibo') . '</span></h2>';
+                                        echo Tecnibo_Portfolio::get_grid_products_projects( $args );
+                                        echo '</section>';
+                                    }
+                                    
+                                    wp_reset_query();
+                                    
+                                    // Projects
+                                    $args = array(
+                                        'post_type' => 'tecnibo_project',
+                                        'orderby' => 'title',
+                                        'posts_per_page' => -1,
+                                        'order' => 'ASC',
+                                        'tax_query' => array(
+                                            array('taxonomy' => 'product_category',
+                                                'field' => 'term_id',
+                                                'terms' => $wp_query->queried_object->term_id //6 //198
+                                            )
+                                        ),
+                                    );      
+                                    if ( Tecnibo_Portfolio::category_has_product_project($args)){
+                                        echo '<div class="divider"><hr class="flush"></div>';
+                                        echo '<section class="tecnibo-row">';
+                                        echo '<h2 class="related_products_projects"><span>' . __('Projects','tecnibo') . '</span></h2>';
+                                        echo Tecnibo_Portfolio::get_grid_products_projects( $args );
+                                        echo '</section>';
+                                    }
+                                   
+                                    wp_reset_query();                                    
                                     endif;// queried object 
                                     ?>
                             <?php 
                                 else:
-                                    _e( 'No Products for this category', 'tecnibo' );
+                                    echo '<p>'.__( "Sorry, no products or projects  matched your criteria.", "tecnibo") .'</p>';
                             endif; // has posts 
-                            wp_reset_query();
+                            
                             ?>
                             
                             
